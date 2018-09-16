58360161356b3280e6d142279e0beaa18ba2a983
Cloud <cloud@cloud-macbook-pro.local>
2018-09-16 13:02:23 +0800

    fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,129 @@
 /*global jQuery */
 
+var Typer = function(element) {
+    this.element = element;
+    var delim = element.dataset.delim || ","; // default to comma
+    var words = element.dataset.words || "override these,sample typing";
+    this.words = words.split(delim).filter(function(v){return v;}); // non empty words
+    this.delay = element.dataset.delay || 200;
+    this.loop = element.dataset.loop || "true";
+    this.deleteDelay = element.dataset.deletedelay || element.dataset.deleteDelay || 800;
+
+    this.progress = { word:0, char:0, building:true, atWordEnd:false, looped: 0 };
+    this.typing = true;
+
+    var colors = element.dataset.colors || "black";
+    this.colors = colors.split(",");
+    this.element.style.color = this.colors[0];
+    this.colorIndex = 0;
+
+    this.doTyping();
+};
+
+Typer.prototype.start = function() {
+    if (!this.typing) {
+        this.typing = true;
+        this.doTyping();
+    }
+};
+Typer.prototype.stop = function() {
+    this.typing = false;
+};
+Typer.prototype.doTyping = function() {
+    var e = this.element;
+    var p = this.progress;
+    var w = p.word;
+    var c = p.char;
+    var currentDisplay = [...this.words[w]].slice(0, c).join("");
+    p.atWordEnd = false;
+    if (this.cursor) {
+        this.cursor.element.style.opacity = "1";
+        this.cursor.on = true;
+        clearInterval(this.cursor.interval);
+        var itself = this.cursor;
+        this.cursor.interval = setInterval(function() {itself.updateBlinkState();}, 400);
+    }
+
+    e.innerHTML = currentDisplay;
+
+    if (p.building) {
+        if (p.char == [...this.words[w]].length) {
+            p.building = false;
+            p.atWordEnd = true;
+        } else {
+            p.char += 1;
+        }
+    } else {
+        if (p.char == 0) {
+            p.building = true;
+            p.word = (p.word + 1) % this.words.length;
+            this.colorIndex = (this.colorIndex + 1) % this.colors.length;
+            this.element.style.color = this.colors[this.colorIndex];
+        } else {
+            p.char -= 1;
+        }
+    }
+
+    if(p.atWordEnd) p.looped += 1;
+
+    if(!p.building && (this.loop == "false" || this.loop <= p.looped) ){
+        this.typing = false;
+    }
+
+    var myself = this;
+    setTimeout(function() {
+        if (myself.typing) { myself.doTyping(); };
+    }, p.atWordEnd ? this.deleteDelay : this.delay);
+};
+
+var Cursor = function(element) {
+    this.element = element;
+    this.cursorDisplay = element.dataset.cursordisplay || "_";
+    element.innerHTML = this.cursorDisplay;
+    this.on = true;
+    element.style.transition = "all 0.1s";
+    var myself = this;
+    this.interval = setInterval(function() {
+        myself.updateBlinkState();
+    }, 400);
+}
+Cursor.prototype.updateBlinkState = function() {
+    if (this.on) {
+        this.element.style.opacity = "0";
+        this.on = false;
+    } else {
+        this.element.style.opacity = "1";
+        this.on = true;
+    }
+}
+
+function TyperSetup() {
+    var typers = {};
+    var elements = document.getElementsByClassName("typer");
+    for (var i = 0, e; e = elements[i++];) {
+        typers[e.id] = new Typer(e);
+    }
+    var elements = document.getElementsByClassName("typer-stop");
+    for (var i = 0, e; e = elements[i++];) {
+        let owner = typers[e.dataset.owner];
+        e.onclick = function(){owner.stop();};
+    }
+    var elements = document.getElementsByClassName("typer-start");
+    for (var i = 0, e; e = elements[i++];) {
+        let owner = typers[e.dataset.owner];
+        e.onclick = function(){owner.start();};
+    }
+
+    var elements2 = document.getElementsByClassName("cursor");
+    for (var i = 0, e; e = elements2[i++];) {
+        let t = new Cursor(e);
+        t.owner = typers[e.dataset.owner];
+        t.owner.cursor = t;
+    }
+}
+
+TyperSetup();
+
 
 /*!
 * Lettering.JS 0.7.0
@@ -495,42 +619,49 @@
         }, 4000);
 
         var matrix = document.getElementById('matrix')
-        var context = matrix.getContext('2d')
-        matrix.height = matrix.offsetHeight
-        matrix.width = matrix.offsetWidth
-        var drop = []
-        var font_size = 16
-        var columns = matrix.width / font_size
-        for (var i = 0; i < columns; i++)
-            drop[i] = 1;
 
-        function drawMatrix() {
+        if(matrix) {
+            var context = matrix.getContext('2d')
+            matrix.height = matrix.offsetHeight
+            matrix.width = matrix.offsetWidth
+            var drop = []
+            var font_size = 16
+            var columns = matrix.width / font_size
+            for (var i = 0; i < columns; i++)
+                drop[i] = 1;
 
-            context.fillStyle = 'rgba(0, 0, 0, 0.1)'
-            context.fillRect(0, 0, matrix.width, matrix.height)
+            function drawMatrix() {
 
-            context.fillStyle = 'green'
-            context.font = font_size + 'px'
-            for (var i = 0; i < columns; i++) {
-                context.fillText(Math.floor(Math.random() * 2), i * font_size, drop[i] * font_size)
-                /*get 0 and 1*/
+                context.fillStyle = 'rgba(0, 0, 0, 0.1)'
+                context.fillRect(0, 0, matrix.width, matrix.height)
 
-                if (drop[i] * font_size > (matrix.height * 2 / 3) && Math.random() > 0.85)/*reset*/
-                    drop[i] = 0
-                drop[i]++
+                context.fillStyle = 'green'
+                context.font = font_size + 'px'
+                for (var i = 0; i < columns; i++) {
+                    context.fillText(Math.floor(Math.random() * 2), i * font_size, drop[i] * font_size)
+                    /*get 0 and 1*/
+
+                    if (drop[i] * font_size > (matrix.height * 2 / 3) && Math.random() > 0.85)/*reset*/
+                        drop[i] = 0
+                    drop[i]++
+                }
             }
+
+            setInterval(drawMatrix, 40);
+
+            setInterval(function(){
+                var $active = $('.our-core .item.active');
+                var $next = $active.next().length > 0 ? $active.next() : $('.our-core .item').eq(0);
+
+                $active.removeClass('active');
+                $next.addClass('active');
+
+            }, 3000);
+
         }
 
-        setInterval(drawMatrix, 40);
 
-        setInterval(function(){
-            var $active = $('.our-core .item.active');
-            var $next = $active.next().length > 0 ? $active.next() : $('.our-core .item').eq(0);
 
-            $active.removeClass('active');
-            $next.addClass('active');
-
-        }, 3000);
 
 
 
@@ -538,4 +669,64 @@
 
 
     });
+
+    window.onload = function() {
+        var mapLength = document.getElementsByTagName('circle').length - 1;
+
+        if(mapLength <= 0)
+            return false;
+
+        var currentCircle = "";
+        var previousCircle = "";
+
+        function rnd(a, z) {
+            return Math.floor(Math.random() * (z - a) + a);
+        }
+
+
+        var randomCircle = rnd(0, mapLength);
+        currentCircle = randomCircle;
+        var a = document.getElementsByTagName('circle')[currentCircle];
+        previousCircle = currentCircle;
+
+        var circleCloneFirst = a.cloneNode(true);
+        var circleCloneSecond = a.cloneNode(true);
+        var circleCloneThird = a.cloneNode(true);
+        a.classList.add("active");
+        circleCloneFirst.classList.add("active-first");
+        circleCloneSecond.classList.add("active-second");
+        circleCloneThird.classList.add("active-third");
+        circleCloneFirst.classList.remove("active");
+        circleCloneSecond.classList.remove("active");
+        circleCloneThird.classList.remove("active");
+        circleCloneFirst.setAttribute('r', 10);
+        circleCloneSecond.setAttribute('r', 14);
+        circleCloneThird.setAttribute('r', 18);
+        document.getElementsByClassName('b-header__map')[0].appendChild(circleCloneFirst);
+        document.getElementsByClassName('b-header__map')[0].appendChild(circleCloneSecond);
+        document.getElementsByClassName('b-header__map')[0].appendChild(circleCloneThird);
+
+
+        (function(){
+
+            if (previousCircle!="") {
+                document.getElementsByTagName('circle')[previousCircle].classList.remove("active");
+            }
+            var randomCircle = rnd(0, mapLength);
+            currentCircle = randomCircle;
+            var a = document.getElementsByTagName('circle')[currentCircle];
+            a.classList.add("active");
+
+            circleCloneFirst.setAttribute('cx', a.getAttribute('cx'));
+            circleCloneSecond.setAttribute('cx', a.getAttribute('cx'));
+            circleCloneThird.setAttribute('cx', a.getAttribute('cx'));
+            circleCloneFirst.setAttribute('cy', a.getAttribute('cy'));
+            circleCloneSecond.setAttribute('cy', a.getAttribute('cy'));
+            circleCloneThird.setAttribute('cy', a.getAttribute('cy'));
+
+            previousCircle = currentCircle;
+            setTimeout(arguments.callee, 3000);
+        })();
+    }
+
 })(jQuery);
